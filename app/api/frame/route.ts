353a85ce455f256b8d7c496abacf21bb7ee219db
Valerie <valdaraa@protonmail.com>
2024-01-30 18:26:28 -0500

    -

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -6,10 +6,9 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://meditation-frame.vercel.app/breathe.gif" />
-    <meta property="fc:frame:button:1" content="Stop" />
-    <meta property="fc:frame:post_url" content="https://meditation-frame.vercel.app/" />
+    <meta property="fc:frame:button:1:action" content="navigate"/> 
+    <meta property="fc:frame:button:1:url" content="/"/>
   </head></html>`);
-
 
 }
 
