8b973bbe19fee47c5c8f34ff5b8749dfcc535403
Valerie <valdaraa@protonmail.com>
2024-01-30 17:51:01 -0500

    try post redirect

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -10,6 +10,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
         <meta property="fc:frame" content="vNext" />
         <meta property="fc:frame:image" content="https://meditation-frame.vercel.app/breathe.gif" />
         <meta property="fc:frame:button:1" content="Stop" />
+        <meta property="fc:frame:button:1:action" content="post_redirect" />
         <meta property="fc:frame:button:1:url" content="https://meditation-frame.vercel.app/" />
         <meta property="fc:frame:post_url" content="https://meditation-frame.vercel.app/api/frame" />
     </head>
