2321ebfb00170705a90e76e3b8c72bf0f02242f9
Valerie <valdaraa@protonmail.com>
2024-01-29 22:35:52 -0500

    rollback changes

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,39 +1,19 @@
-// Importing necessary modules
 import { getFrameAccountAddress } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
 
-// Function to handle button click and toggle the state
-function toggleButtonState(currentState: string): string {
-  return currentState === 'Start breathing exercise' ? 'Stop' : 'Start breathing exercise';
-}
-
-// Defining an async function to handle responses
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  // Check if the request includes a parameter indicating the current button state
-  const currentButtonState = (req as any).parameters && (req as any).parameters['fc:frame:button:1'];
 
-  // Determine the new button state and set the image URL accordingly
-  const newButtonState = toggleButtonState(currentButtonState);
-  const imageUrl = newButtonState === 'Stop'
-    ? 'https://meditation-frame.vercel.app/breathe.gif'
-    : 'https://meditation-frame.vercel.app/start.png';
 
-  // Construct the HTML response with dynamic content
-  const htmlContent = `<!DOCTYPE html><html><head>
+  return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${imageUrl}" />
-    <meta property="fc:frame:button:1" content="${newButtonState}" />
+    <meta property="fc:frame:image" content="https://meditation-frame.vercel.app/breathe.gif" />
+    <meta property="fc:frame:button:1" content="Stop" />
     <meta property="fc:frame:post_url" content="https://meditation-frame.vercel.app/api/frame" />
-  </head></html>`;
-
-  // Return the NextResponse with the dynamic HTML content
-  return new NextResponse(htmlContent);
+  </head></html>`);
 }
 
-// Handling POST requests
 export async function POST(req: NextRequest): Promise<Response> {
   return getResponse(req);
 }
 
-// Exporting a constant variable
 export const dynamic = 'force-dynamic';
